39db19c963c65874cab12fc4af3a705a17b62e71
SourcedCMD <cmd.sourced@gmail.com>
2026-07-31 12:28:15 +0000

    LibChange

--- lib/discord-utils.ts
+++ lib/discord-utils.ts
@@ -1,11 +1,11 @@
 // Discord's epoch: 2015-01-01T00:00:00.000Z, in ms since Unix epoch
-const DISCORD_EPOCH = 1420070400000n
+const DISCORD_EPOCH = BigInt(1420070400000)
 
 /**
  * Decodes a Discord snowflake ID into the Date it was created.
  * Useful for spotting newly-created ("alt") accounts in logs.
  */
 export function snowflakeToDate(id: string): Date {
-  const ms = (BigInt(id) >> 22n) + DISCORD_EPOCH
+  const ms = (BigInt(id) >> BigInt(22)) + DISCORD_EPOCH
   return new Date(Number(ms))
 }
